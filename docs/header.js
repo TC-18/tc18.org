54f97069173692085db984e403b27a0958de858b
Kerautret <bertrand.kerautret@univ-lorraine.fr>
2018-07-09 01:19:13 +0200

    debut menu

---
+++ docs/header.js
@@ -0,0 +1,11 @@
+document.getElementById("headerSite").innerHTML = '  \
+<div class="jumbotron" > \
+  <div class="row"> \
+        <div class="col-md-8 col-sm-8 col-xs-12 " > \
+        <h1 class="col-xs-12 "> IAPR - TC18    \
+          <a href="index/html">Discrete Geometry and Mathematical Morphology</a></h1> \
+        </div>   \
+        <div class="col-md-4 col-sm-4 " > \
+       <img class=" d-none d-sm-none d-md-block" width="300" src="Images/IAPR.svg"> \
+       <img class=" d-none d-sm-block d-md-none" width="150" src="Images/IAPR.svg"> </div>\
+        </div> </div> ';
---
+++ docs/menu.js
@@ -0,0 +1,28 @@
+
+
+document.getElementById("navMenu").innerHTML = '<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4"> \
+      <a class="navbar-brand" href="index.html">Scope</a>\
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">\
+	<span class="navbar-toggler-icon"></span>\
+      </button>\
+      <div class="collapse navbar-collapse" id="navbarCollapse">\
+	<ul class="navbar-nav mr-auto">\
+	  <li class="navbar-brand dropdown active">\
+	    <a class="nav-link dropdown-toggle" href="http://example.com"  \
+               id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Organisation</a>\
+	    <div class="dropdown-menu" aria-labelledby="dropdown01">\
+	      <a class="dropdown-item" href="modernite.html">For members</a>\
+	      <a class="dropdown-item" href="tradition.html">How to become a member</a>\
+	      <a class="dropdown-item" href="design.html">List of members</a>\
+	    </div>\
+	  </li>\
+	  <li class="navbar-brand active">\
+	    <a class="nav-link" href="tarifs.html">Tarifs</a>\
+	  </li>\
+	  <li class="navbar-brand">\
+	    <a class="nav-link active" href="contacts.html">Contact</a>\
+	  </li>\
+	</ul>\
+	</div>';
+
+
