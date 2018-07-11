e5a5daee232fa28a69424dd28afc2a846ce5a288
Kerautret <bertrand.kerautret@univ-lorraine.fr>
2018-07-11 10:28:54 +0200

    fix button responsive

--- docs/members.js
+++ docs/members.js
@@ -2,7 +2,7 @@ document.getElementById("memberslist").innerHTML = '\
 <div class="card"> \
     <div class="card-header" id="headingOne"> \
       <h5 class="mb-0"> \
-        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">\
+        <button class="btn btn-link collapsed btn-responsive" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">\
           TC18 Members (click to uncollapse the <a id="demo"></a>) \
         </button> \
       </h5>\
