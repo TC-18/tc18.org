f4a9724ef7eac9ff839aa6d62f621c9054b2209f
Kerautret <bertrand.kerautret@univ-lorraine.fr>
2018-07-11 15:42:07 +0200

    sticky-top barnav

--- docs/menu.js
+++ docs/menu.js
@@ -1,6 +1,6 @@
 
 
-document.getElementById("navMenu").innerHTML = '<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4"> \
+document.getElementById("navMenu").innerHTML = ' \
       <a class="navbar-brand" href="scope.html">Scope</a>\
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">\
 	<span class="navbar-toggler-icon"></span>\
