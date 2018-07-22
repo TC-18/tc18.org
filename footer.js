fbe61001d0b7915f7938a43b089d41d1e69d8804
Kerautret <bertrand.kerautret@univ-lorraine.fr>
2018-07-22 10:29:41 +0200

    add link to edit individual pages 🍾

--- footer.js
+++ footer.js
@@ -1,11 +1,9 @@
+var pagename = location.pathname.substring(location.pathname.lastIndexOf("/") + 1);
 document.getElementById("footerSite").innerHTML = '  \
 <hr /> \
 <div class="footer"> \
 <p><a href="index.php">IAPR-TC18 webpage</a> \
-You can conribute to the edtion trough the <a href="https://github.com/TC-18/tc18.org">Github source page</a> </p>\
+You can conribute to the edtion trough the <a href="https://github.com/TC-18/tc18.org">Github source page</a> and directly edit this page <a href="https://github.com/TC-18/tc18.org/blob/master/'+pagename+'">here</a>. </p>\
 </div> \
 ';
-
-
-
 
