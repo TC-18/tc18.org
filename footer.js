f0f55403d69f65e405e95d50cebfc0fcfb0b3d24
Bertrand Kerautret <bertrand.kerautret@univ-lorraine.fr>
2018-07-23 23:01:35 +0200

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,6 +1,6 @@
 var path = window.location.pathname;
 var pagename = path.split("/").pop();
-if (pagename=="tc18.org" || pagename=="www.tc18.org")
+if (!pagename || 0 === pagename.length|| pagename=="tc18.org" || pagename=="www.tc18.org" || !pagename || 0 === pagename.length)
 {
   pagename="index.html"; 
 }
