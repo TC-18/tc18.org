0fa34b9ba98cf75c3b5ff2f4f5294fb5a54c1b40
Bertrand Kerautret <bertrand.kerautret@univ-lorraine.fr>
2018-07-24 22:58:31 +0200

    Update footer.js

--- footer.js
+++ footer.js
@@ -7,7 +7,7 @@ if (!pagename || 0 === pagename.length|| pagename=="tc18.org" || pagename=="www.
 document.getElementById("footerSite").innerHTML = '  \
 <hr /> \
 <div class="footer"> \
-<p><a href="index.php">IAPR-TC18 webpage</a> \
+<p><a href="index.html">IAPR-TC18 webpage</a> \
 You can contribute to the edition through the <a href="https://github.com/TC-18/tc18.org">Github source page</a> and directly edit this page <a href="https://github.com/TC-18/tc18.org/blob/master/'+pagename+'">here</a>. </p>\
 </div> \
 ';
